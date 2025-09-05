cc6144205f537fe09b2028b42da925fda0e9a837
timo <44401485+Timo972@users.noreply.github.com>
2025-09-05 17:28:28 +0200

    chore: update build script and remove docker-compose

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 docker build -t droidrun/droidrun-android-world:latest --platform linux/amd64 "$@" .
+docker build -t droidrun/ws-scrcpy:latest --platform linux/amd64 "$@" -f Dockerfile.ws-scrcpy .
+docker build -t droidrun/android-world:latest --platform linux/amd64 "$@" ./android_world
